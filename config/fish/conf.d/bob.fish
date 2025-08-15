0f9187336bce6144fc61ae1f5cf60fd18ef4538b
5yndr0m <sdilanjana21@gmail.com>
2025-08-16 00:40:13 +0530

    feat: going for a simpler fish set up

---
+++ config/fish/conf.d/bob.fish
@@ -0,0 +1,1 @@
+source "/home/syndrom/.local/share/bob/env/env.fish"
---
+++ config/fish/functions/fish_greeting.fish
@@ -0,0 +1,18 @@
+function fish_greeting
+    # Color for readability
+    # printf '\e[38;5;250m'
+
+    # # Fetch and display quote
+    # echo
+    # set_color yellow
+
+    # set response (curl -s https://programming-quotesapi.vercel.app/api/random)
+    # # Parse JSON safely
+    # set quote (echo $response | jq -r '.en + " — " + .author' 2>/dev/null)
+    # if test -z "$quote"
+    #     set quote '"Code is like humor. When you have to explain it, it’s bad." — Cory House'
+    # end
+    # echo "$quote"
+
+    # set_color normal
+end
